be02cdcba24bcc48e388d29b7d8487a620e6d468
Talmaciu Elena <148704214+ellat99@users.noreply.github.com>
2024-03-31 12:45:24 +0300

    partefinalaregister

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,89 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { PrivateRoute } from '../routesConfig/PrivateRoute';
+import { RestrictedRoute } from '../routesConfig/RestrictedRoute';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsRefreshing } from '../reduxConfig/auth/selectors';
+import { refreshUser } from '../reduxConfig/auth/operations';
+import { useMediaQuery } from 'react-responsive';
+import Loader from './Loader/Loader';
+import { selectIsLoading } from '../reduxConfig/global/selectors';
+import LoginPage from '../pages/LoginPage';
+import RegistrationPage from '../pages/RegistrationPage';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 60,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const isTabletOrDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader visible={isLoading} />
+  ) : (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route
+            index
+            element={
+              <RestrictedRoute
+                redirectTo="/dashboard"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute
+                redirectTo="/dashboard"
+                component={<RegistrationPage />}
+              />
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute
+                redirectTo="/dashboard"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="dashboard"
+            element={
+              <PrivateRoute
+                redirectTo="/login"
+                component={<div>Dashboard Page</div>}
+              />
+            }
+          >
+            <Route index element={<div>Home Page</div>} />
+            <Route path="home" element={<div>Home Page</div>} />
+            <Route path="statistics" element={<div>Statistics Page</div>} />
+            <Route
+              path="currency"
+              element={
+                isTabletOrDesktop ? (
+                  <Navigate to="/" />
+                ) : (
+                  <div>Currency Page</div>
+                )
+              }
+            />
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Loader visible={isLoading} />
+    </>
   );
 };
+
+export default App;
---
+++ src/reduxConfig/currency/operations.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { currencyAPI } from '../../configAxios/currencyApi';
+
+const currencyID = {
+  usd: 'USD',
+  eur: 'EUR',
+};
+
+export const fetchCurrency = createAsyncThunk(
+  'fetchCurrency',
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await currencyAPI.get('/bank/currency');
+      if (res.data && res.data.length > 0) {
+        const filteredArray = res.data.filter(
+          el =>
+            (el.currencyCodeA === 840 && el.currencyCodeB === 980) ||
+            (el.currencyCodeA === 978 && el.currencyCodeB === 980)
+        );
+        const result = filteredArray.map(el => {
+          if (el.currencyCodeA === 840) {
+            return { ...el, currencyName: currencyID.usd };
+          } else if (el.currencyCodeA === 978) {
+            return { ...el, currencyName: currencyID.eur };
+          }
+          return el;
+        });
+        return result;
+      }
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
